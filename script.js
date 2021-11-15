fdbf88132d247b110727d70fd26cdfa6c1d4d608
Shiksha000 <92433900+Shiksha000@users.noreply.github.com>
2021-11-15 20:40:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+    $(Window).on('load scroll',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+        
+        if($(Window).scrollTop() > 0){
+            $('#scroll-top').show();
+        }else{
+            $('#scroll-top').hide();
+        }
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+          scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+
+    });
+});
